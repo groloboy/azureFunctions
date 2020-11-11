3e1c1d88278b923888ca425f36ad2ca83bb53a53
Brayan Andrés Quiñonez Ramirez <bquinones@opalo.com.co>
2020-11-10 22:20:05 -0500

    fixes

--- comands_vagrant.sh
+++ comands_vagrant.sh
@@ -22,8 +22,8 @@ sudo apt-get update
 sudo apt-get install azure-functions-core-tools-3
 
 sudo apt-get update; \
- sudo apt-get install -y apt-transport-https &amp;&amp; \
- sudo apt-get update &amp;&amp; \
+ sudo apt-get install -y apt-transport-https && \
+ sudo apt-get update && \
  sudo apt-get install -y dotnet-sdk-3.1
 
 mkdir testAzureFunctions
@@ -42,7 +42,7 @@ vim search.cs
 "La función de activación HTTP de C # procesó una solicitud"
 # obtener todas las personas http://192.168.100.3:7071/api/Search?name
 # obtener una persona http://192.168.100.3:7071/api/Search?name=lucas
-
+############### configurar anonymus en search.cs ##############
 sudo docker build -t myprojectname .
 sudo docker images
 sudo docker run -p 8080:80 myprojectname
@@ -62,7 +62,8 @@ az functionapp plan create --resource-group AzureFunctionsContainers-rg --name m
 # cambiar nombre bquinonesFuncAzure
 az functionapp create --name bquinonesFuncAzure --storage-account bquinonesufuncstorage --resource-group AzureFunctionsContainers-rg --plan myPremiumPlan --runtime dotnet --deployment-container-image-name groloboy/myprojectname
 az storage account show-connection-string --resource-group AzureFunctionsContainers-rg --name bquinonesufuncstorage --query connectionString --output tsv
-az functionapp config appsettings set --name bquinonesFuncAzure --resource-group AzureFunctionsContainers-rg --settings AzureWebJobsStorage=DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=bquinonesufuncstorage;AccountKey=ZrjRY/rla+oNrQB3XS46Qck/aly/3G5Z2mwjb3gWSagT12wlfxBgnIAl3Tl8rPcZTR0naC1wcHfcJh/0Q2BKlQ==
+az functionapp config appsettings set --name bquinonesFuncAzure --resource-group AzureFunctionsContainers-rg --settings AzureWebJobsStorage=DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=bquinonesufuncstorage;AccountKey=Q+9Jwz8rpmoL2JG0z+g5i3fAa+il6g6bkVnMluToRFxI97z7kX/puTFqBBGya8E/mFktkt1PShsKDyGf7XdfbQ==
+
 
 # obtener todas las personas https://bquinonesfuncazure.azurewebsites.net/api/Search?name
 # obtener una persona https://bquinonesfuncazure.azurewebsites.net/api/Search?name=lucas
@@ -80,17 +81,15 @@ git clone https://github.com/awssimplified/RESTApiNodeJS.git
 
 sudo docker build -t nodeapirest .
 sudo docker images
-sudo docker run -it -p 3000:300 --name=nodeapi nodeapirest
+sudo docker run -it -p 3000:3000 --name=nodeapi nodeapirest
 # curl -d 'user=admin&name=admin&email=admin@gmail.com' -X POST http://192.168.100.3:3000/users
 # curl -d 'user=admin&name=admin2' -X PUT http://192.168.100.3:3000/users?user=admin
 # curl -X DELETE http://192.168.100.3:3000/users?user=user1
-sudo docker login
+
 #cambiar nombre groloboy/nodeapirest
 sudo docker tag nodeapirest groloboy/nodeapirest
 sudo docker push groloboy/nodeapirest
 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az group create --name AzureFunctionsContainers-rg --location centralus
 # cambiar nombre bquinonesreststorage
 az storage account create --name bquinonesreststorage --location westeurope --resource-group AzureFunctionsContainers-rg --sku Standard_LRS
 az functionapp plan create --resource-group AzureFunctionsContainers-rg --name myPremiumPlan --location centralus --number-of-workers 1 --sku EP1 --is-linux
@@ -98,7 +97,11 @@ az functionapp plan create --resource-group AzureFunctionsContainers-rg --name m
 # cambiar nombre bquinonesRestAzure
 az functionapp create --name bquinonesRestAzure --storage-account bquinonesreststorage --resource-group AzureFunctionsContainers-rg --plan myPremiumPlan --runtime dotnet --deployment-container-image-name groloboy/nodeapirest
 az storage account show-connection-string --resource-group AzureFunctionsContainers-rg --name bquinonesreststorage --query connectionString --output tsv
-az functionapp config appsettings set --name bquinonesRestAzure --resource-group AzureFunctionsContainers-rg --settings AzureWebJobsStorage=DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=bquinonesreststorage;AccountKey=ZrjRY/rla+oNrQB3XS46Qck/aly/3G5Z2mwjb3gWSagT12wlfxBgnIAl3Tl8rPcZTR0naC1wcHfcJh/0Q2BKlQ==
+az functionapp config appsettings set --name bquinonesRestAzure --resource-group AzureFunctionsContainers-rg --settings AzureWebJobsStorage=DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=bquinonesreststorage;AccountKey=lJpnXqEZc1uXeJ+Z7bYVQ+evzNEezmcwm24xI6o+R7KF5q9E3mfvIoIsiO3vsLs7az9jdHCApy4qqT6bc++6xg==
 
-# obtener todas las personas https://bquinonesRestAzure.azurewebsites.net/api/Search?name
-# obtener una persona https://bquinonesRestAzure.azurewebsites.net/api/Search?name=lucas
+# obtener todas las personas https://bquinonesRestAzure.azurewebsites.net/users
+# obtener una persona https://bquinonesRestAzure.azurewebsites.net/users?name=lucas
+
+# curl -d 'user=admin&name=admin&email=admin@gmail.com' -X POST https://bquinonesRestAzure.azurewebsites.net/users
+# curl -d 'user=admin&name=admin2' -X PUT https://bquinonesRestAzure.azurewebsites.net/users?user=admin
+# curl -X DELETE https://bquinonesRestAzure.azurewebsites.net/users?user=user1
